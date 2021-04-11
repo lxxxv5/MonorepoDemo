f5318eab388e3b86fa64ffa4a72c7ffe8908ad93
lxxxv5 <1490769790@qq.com>
2021-04-11 15:57:13 +0800

    button add disabled

--- packages/button/src/button.js
+++ packages/button/src/button.js
@@ -3,8 +3,12 @@ import utils from '@lxxxv5-lerna-demo/utils'
 
 const { isArray } = utils
 
-const Button = (text, children) => {
-  return <button>{isArray(text) ? text.join(',') : children}</button>
+const Button = (text, disabled, children) => {
+  return (
+    <button disabled={disabled}>
+      {isArray(text) ? text.join(',') : children}
+    </button>
+  )
 }
 
 export default Button
